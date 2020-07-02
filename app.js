0c6219c829c8cf21eac1156854b8a99827348ba7
Ewerton Silveira <Ewerton.developer@outlook.com>
2020-07-03 11:42:09 +1200

    fixed layout

--- app.js
+++ app.js
@@ -15,7 +15,29 @@ class ExcellApp {
 
     drawSheet() {
 
-        let sheetMatrix = this.createElement('div');
+        let appMain = this.createElement('div');
+        appMain.id="app-main"
+
+        let mainContent = this.createElement('div');
+        mainContent.id="main-content"
+
+        let sheetSectionContent = this.createElement('div');
+        sheetSectionContent.id="sheet-section"
+
+        let sheetContent = this.createElement('div');
+        sheetContent.id="sheet-content"
+
+        let sheetContentRowIndex = this.createElement('div');
+        sheetContentRowIndex.id="sheet-content-row-index"
+
+        let sheetContentColIndex = this.createElement('div');
+        sheetContentColIndex.id="sheet-content-col-index"
+        let colHeadIndex = this.createElement('div');
+        colHeadIndex.id="sheet-content-col-head-index"
+        
+        let sheetBaseMatrix = this.createElement('div');
+        sheetBaseMatrix.id="sheet-base-matrix"
+
         let columns = this.createElement('div');
         columns.setAttribute('class', "row-column");
 
@@ -32,28 +54,40 @@ class ExcellApp {
             columns.appendChild(colIndex);
         }
 
+        // Add rows to main content [1,2,3,4...]
         for (let index = 1; index <= ROWS_MAX; index++) {
             let rowIndex = `r${index}`;
+            
+            let row = this.createElement('div');
+            row.id=rowIndex;
+            row.setAttribute('class', "row-index");
+            
+            // Add columns to the current row
+            let rowColumns = columns.cloneNode(true)
+            rowColumns.id = `c${rowIndex}`
+            
+            row.appendChild(rowColumns);
+            sheetBaseMatrix.appendChild(row);
+        }
+
+        // Create Content head rows [A,B,C...]
+        for (let index = 1; index <= ROWS_MAX; index++) {
 
             let colIndex = this.createElementWithValue('div', index);
             colIndex.id=`r${index}-c${0}`;
             colIndex.setAttribute('class', "col-index");           
             
-            let row = this.createElement('div');
-            row.id=rowIndex;
-            row.setAttribute('class', "row-index");
-            row.appendChild(colIndex);
-
-            // Add columns to the current row
-            let rowColumns = columns.cloneNode(true)
-            rowColumns.id = `${rowIndex}-${rowColumns.id}`
-
-            row.appendChild(columns.cloneNode(true));
-
-            sheetMatrix.appendChild(row);
+            sheetContentRowIndex.appendChild(colIndex);
         }
-
-        app.appendChild(sheetMatrix);
+        
+        sheetContentColIndex.appendChild(colHeadIndex);
+        sheetContent.appendChild(sheetContentRowIndex);
+        sheetContent.appendChild(sheetBaseMatrix);
+        sheetSectionContent.appendChild(sheetContentColIndex);
+        sheetSectionContent.appendChild(sheetContent);
+        mainContent.appendChild(sheetSectionContent);        
+        appMain.appendChild(mainContent);
+        app.appendChild(appMain);
     }
 
     createElementWithValue(type, value) {
